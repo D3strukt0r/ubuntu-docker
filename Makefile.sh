20ab8473a79b1a554bd55db79a04cddecd4b4ee0
D3strukt0r <dev@d3strukt0r.me>
2022-01-24 02:07:11 +0100

    Add versions

--- Makefile.sh
+++ Makefile.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+set -o errexit -o nounset -o pipefail -o xtrace
+
+# Switch to this file's directory
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
 declare -A aliases=(
 	#[suite]='tag1 tag2 ...'
 )
